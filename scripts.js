caf1707fa0621089111ed803be7cc8233570e7bf
koryan <akoryakin@stream.ru>
2019-09-25 12:25:57 +0300

    refactor

---
+++ scripts.js
@@ -0,0 +1,204 @@
+
+
+
+var layers = {
+	markersLayer: L.layerGroup(),
+	pointsLayer: L.layerGroup(),
+	cities: L.layerGroup(),
+	geometry: L.layerGroup(),
+	clear: function(){
+		this.markersLayer.clearLayers();
+		this.pointsLayer.clearLayers();
+		this.geometry.clearLayers();
+	}
+}
+
+var bigMap = undefined;
+
+
+let maxPointLengthInTooltip = 10;
+let prev = undefined;	
+
+
+let posMethodsColors = {
+	8192: "a00100",
+	4096: "afafaf",
+	2: "123456"
+
+}
+
+//fill inputs 
+// let startParams = {
+// 	msisdn: '+79152103911',
+// 	fromTime: 
+// 	tillTime: 
+// }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	mapInit()
+
+	
+	makeRequest()
+});
+
+console.log(document.getElementById('msisdn'))
+
+
+
+
+
+document.addEventListener('click', function (event) {	
+	if (event.target.matches('#submit')){
+		makeRequest()
+	}
+	if(event.target.matches('#clear')){
+		layers.clear()
+	}
+}, false);
+
+function makeRequest(){
+	let doRequest = function(cb){
+		var request = new XMLHttpRequest();
+		var body = {"sender":"m2m","profile":"poisk","subscriberId":"msisdn"+msisdn,"priority":3,"hideTimes":[{"from":"2018-09-24T03:03:34.477Z","to":"2018-09-24T21:00:00.000Z"}],"sources":["locations"],"inputs":[13001,16384],"infolevel":1,"lang":"ru","startTime":fromTime,"endTime":tillTime}
+
+
+		request.open('POST', "http://10.72.12.11:9005/ldtdpsvc/rest/v0/ldtd/histvals");
+		request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+		request.setRequestHeader('Authorization', 'Basic bGR0ZHB1c2VyMTp7QTMlcn1zb342dmI=');
+		request.setRequestHeader('Accept', 'application/json');
+
+		request.send(JSON.stringify(body));
+
+		request.onreadystatechange = function () {
+			
+
+		    if (request.readyState === 4) {
+		       let ans = JSON.parse(request.responseText)
+		       if(!ans.inputValues){
+		       		cb("No data")
+		       		return
+		       }
+		       cb(false, ans)		       
+
+		    }
+		}
+	}
+
+	layers.clear()
+
+	let msisdn = document.getElementById('msisdn').value
+	let fromTime = document.getElementById('fromTime').value
+	let tillTime = document.getElementById('tillTime').value
+
+	doRequest(function(err, data){
+		if(err){
+			alert(err)
+			return;
+		}
+		draw(data)
+	})
+}
+
+let draw = function(markers){
+	let countRepeats = 1;
+	let usedCoords = {};
+	let index = 0;
+	let lineCoords = [];
+	for ( let i in markers.inputValues)
+	{
+		let currentMarker =  markers.inputValues[i];
+		
+		
+		let coords = [currentMarker.inputs[0].v.latitude, currentMarker.inputs[0].v.longitude,];
+		let radius = currentMarker.inputs[0].v.radius
+		let currentTime = moment(currentMarker.inputs[1].v).format("HH:mm:ss DD.MM")
+
+		
+		//if (""+coords != ""+prev){
+
+			lineCoords.push(coords);
+			index ++;
+
+			if(!usedCoords[""+coords]){
+				usedCoords[""+coords] = [[currentTime, i]];
+			}else{
+				usedCoords[""+coords].push([currentTime, i]);
+			}
+
+			let text = 
+				"<b>Радиус:</b> "+radius+"<br>"+
+				"<b>Время:</b> "+ currentTime
+
+			if(usedCoords[""+coords].length > 1){
+				text+="<br><b>Был там:</b> "+usedCoords[""+coords].length + "<br>"+usedCoords[""+coords].map(el => "<i>"+ el[1] +"</i>) "+ el[0]).join("<br />")+""
+				text = usedCoords[""+coords].map(el => el[1]).join(", ")+"</b><br />"+text
+			}else{
+				text = i+"</b><br />"+text
+			}
+			text = "<b>Точка № "+ text;
+
+			let fillColor =  posMethodsColors[currentMarker.inputs[0].v.pos_method]
+			let strokeColor =  "#"+(parseInt(fillColor, 16)+1000).toString(16);
+			fillColor = "#"+fillColor
+			
+
+			let circle = L.circle([currentMarker.inputs[0].v.latitude, currentMarker.inputs[0].v.longitude],{radius: radius, weight: .5, color: strokeColor, fillColor: fillColor, fillOpacity: .05});
+			let cCenter = L.circleMarker([currentMarker.inputs[0].v.latitude, currentMarker.inputs[0].v.longitude],{radius: 5, weight: 1, color: "white", fillColor: "green"});
+
+
+			//if (usedCoords[""+coords].length > maxPointLengthInTooltip)
+			cCenter.bindPopup(text).bindTooltip(text, {className: 'myTooltip'})
+			circle.bindTooltip(text, {className: 'myTooltip'})
+			circle.addTo(layers.markersLayer)
+			cCenter.addTo(layers.pointsLayer)
+			
+			timeStart = currentTime
+			
+		//}	
+		countRepeats = 1;
+		prev = ""+coords;
+		//if(index == 7)break;
+
+
+	}
+
+	var polyline = L.polyline(lineCoords, {color: 'red', weight:1}).addTo(layers.geometry);
+	bigMap.fitBounds(polyline.getBounds());
+	
+	//L.geoJSON({"type": "LineString",	"coordinates":lineCoords}).addTo(layers.geometry);
+
+	
+	
+}
+
+var mapInit = function(){
+	var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+	var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light'}),
+		streets  = L.tileLayer(mbUrl, {id: 'mapbox.streets'});
+
+
+	bigMap = L.map('map', {
+		center: [55.751244, 37.618423],
+		zoom: 12,
+		layers: [grayscale, layers.cities, layers.geometry, layers.markersLayer, layers.pointsLayer]
+	});
+
+	var baseLayers = {
+		"Grayscale": grayscale,
+		"Streets": streets
+	};
+
+	var overlays = {
+		"Сырой трек": layers.geometry,
+		"Сырые радиусы":layers.markersLayer,
+		"Сырые точки":layers.pointsLayer
+	};
+	
+
+	L.control.layers(baseLayers, overlays).addTo(bigMap);
+
+	
+}
+
